44379c3b740c52edd1453525a9c9958f9944ac4c
Mansouri Hosein <h.m4571@gmail.com>
2018-09-23 10:12:59 +0330

    Move To Github

---
+++ api/routes/products.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({
+        message : 'Handling GET request products'
+    });
+});
+
+router.post('/',(req,res,next)=>{
+    const product = {
+        name : req.body.name,
+        price : req.body.price
+    };
+    res.status(201).json({
+        message : 'Handling POST request products',
+        createdProduct: product
+    });
+});
+
+router.patch('/:productId',(req,res,next)=>{  
+    res.status(200).json({
+        message : 'Updated product!'
+    });
+});
+
+router.delete('/:productId',(req,res,next)=>{  
+    res.status(200).json({
+        message : 'Deleted product!'
+    });
+});
+
+module.exports = router;
